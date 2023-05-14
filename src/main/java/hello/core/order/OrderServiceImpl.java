40c14545973f339498d63bef2fb3e13f2fbb6759
truelightwater <truelightwater@gmail.com>
2023-05-14 21:49:07 +0900

    의존관계 자동/수동

--- src/main/java/hello/core/order/OrderServiceImpl.java
+++ src/main/java/hello/core/order/OrderServiceImpl.java
@@ -1,13 +1,16 @@
 package hello.core.order;
 
+import hello.core.annotation.MainDiscountPolicy;
 import hello.core.discount.DiscountPolicy;
 import hello.core.member.Member;
 import hello.core.member.MemberRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 @Component
-@RequiredArgsConstructor // lombok : final 이 붙은 필드를 모아서 생성자를 자동으로 사용하게 한다.
+// @RequiredArgsConstructor // lombok : final 이 붙은 필드를 모아서 생성자를 자동으로 사용하게 한다.
 public class OrderServiceImpl implements OrderService {
 
     // (필수) final : 무조건 값이 있어야 한다 !
@@ -15,11 +18,15 @@ public class OrderServiceImpl implements OrderService {
     private final MemberRepository memberRepository;
     private final DiscountPolicy discountPolicy;
 
-/*    @Autowired // 생성자 주입
-    public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
+
+    /*@Autowired
+    private DiscountPolicy rateDiscountPolicy; // 조회 대상 빈이 2개 이상일때, 필드 명 매칭으로 지정할 수 있다.*/
+
+    @Autowired
+    public OrderServiceImpl(MemberRepository memberRepository, @MainDiscountPolicy DiscountPolicy discountPolicy) {
         this.memberRepository = memberRepository;
         this.discountPolicy = discountPolicy;
-    }*/
+    }
 
     // 테스트 용도
     public MemberRepository getMemberRepository() {
